459de40f1c6ba5fcc5516299af86c257b14c67c7
Ishiukwu <samuelogoegbu@gmail.com>
2023-02-16 12:43:58 -0800

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
-
+/**
+ * main - A simple function that prints outs a line of code
+ *
+ * Return:0 (Success)
+ */
 int main(void)
- { 
-     puts("\"Programming is like building a multilingual puzzle")
-     return (0);
-
- }
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
