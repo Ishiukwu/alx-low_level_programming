4103e8262a4d8ba61459017b3de2e025163b9872
Samuel Ogoegbu <samuelogoegbu@gmail.com>
2023-06-15 13:40:33 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/** mai -Entry point
- * 
+/**
+ * main -Entry point
  * Return: Always 0(Success)
  */
-int main(void){
-	puts("Programming is like building a multilingual puzzle\n")
-	return (0)
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
