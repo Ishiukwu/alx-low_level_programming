60c021407a54952d9611d4a167642e1a28a761b2
Samuel Ogoegbu <samuelogoegbu@gmail.com>
2023-06-16 19:37:44 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int a;
+
+	for (a = 0; a < 26; a++)
+	{
+		putchar(alphabet[a]);
+	}
+	putchar("\n");
+	return (0);
+}
