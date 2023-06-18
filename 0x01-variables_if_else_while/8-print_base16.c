9ba77869982043cea12a6e0d489defd6ecdb456e
Samuel Ogoegbu <samuelogoegbu@gmail.com>
2023-06-17 23:16:24 -0700

    16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: - Always 0(Success)
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 48; j < 58; j++)
+	{
+		putchar(j);
+	}
+	for (j = 97; j < 103; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
